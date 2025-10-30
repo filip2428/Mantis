674ee2edacd9e7be67a657be738fb68725528678
filip2428 <165894116+filip2428@users.noreply.github.com>
2025-10-30 11:20:22 +0200

    minor navbar fix

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -167,7 +167,7 @@ export const NavItems = ({
               </button>
             ) : (
               <Link
-                href={item.link}
+                href={item.link ?? "/"}
                 onClick={onItemClick}
                 className="relative z-20 rounded-full px-4 py-2 text-mantis-green-700 transition-colors hover:bg-mantis-cream/80 hover:text-mantis-green-800 dark:text-neutral-200 dark:hover:bg-[#1b3525]"
               >
